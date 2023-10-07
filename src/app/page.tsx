7109624a17aaeb51114775bb74a8753f33268fe6
Jongbum <amjong2@gmail.com>
2023-10-07 14:49:10 +0900

    app/page.tsx: Added SearchBar at home page to handle user input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import SearchBar from './components/SearchBar';
+
 export default function Home() {
+  const [searchInput, setSearchInput] = useState<string>('');
   return (
     <main>
       <section className='flex flex-col min-h-screen justify-center items-center'>
         유투브 영상 링크
         <br />
-        <input></input>
+        <SearchBar onSubmit={() => setSearchInput}></SearchBar>
       </section>
     </main>
   );
