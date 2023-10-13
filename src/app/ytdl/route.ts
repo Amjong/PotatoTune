8b4b43844e0d26c174e2cd1a3856bb98800de055
Jongbum <amjong2@gmail.com>
2023-10-13 17:02:29 +0900

    ytdl/route.ts: create audio file from youtube link by using ytdl-core

--- src/app/ytdl/route.ts
+++ src/app/ytdl/route.ts
@@ -2,9 +2,30 @@ import { type NextRequest } from 'next/server';
 import ytdl from 'ytdl-core';
 import fs from 'fs';
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const url = searchParams.get('url');
-  ytdl(url).pipe(fs.createWriteStream(`video.mp3`));
+  const videoId = ytdl.getURLVideoID(url);
+
+  ytdl
+    .getInfo(videoId)
+    .then((info: any) => {
+      // Select the video format and quality
+      const format = ytdl.chooseFormat(info.formats, {
+        quality: 'highestaudio',
+      });
+      // Create a write stream to save the video file
+      const outputFilePath = `${info.videoDetails.title}.mp3`;
+      const outputStream = fs.createWriteStream(outputFilePath);
+      // Download the video file
+      ytdl.downloadFromInfo(info, { format: format }).pipe(outputStream);
+      // When the download is complete, show a message
+      outputStream.on('finish', () => {
+        console.log(`Finished downloading: ${outputFilePath}`);
+      });
+    })
+    .catch((err: any) => {
+      console.error(err);
+    });
   return new Response();
 }
